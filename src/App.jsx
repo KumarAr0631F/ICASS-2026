14dc5768100c7b76a8699d8997bcc52f8f59d598
Nitin Kumar <98699025+KumarAr0631F@users.noreply.github.com>
2025-01-16 17:08:59 +0530

    removing tracks

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Registration from "./pages/Registration";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Event from "./pages/Event";
-import Track from "./pages/Track";
 
 const App = () => {
   return (
@@ -19,7 +18,6 @@ const App = () => {
         <Route path={"/guidelines"} element={<Guideline />} />
         <Route path={"/registration"} element={<Registration />} />
         <Route path={"/events"} element={<Event />} />
-        <Route path={"/tracks-and-topics"} element={<Track />} />
       </Routes>
       <Footer />
     </div>
