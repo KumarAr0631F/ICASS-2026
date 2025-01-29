a55e5356c161db059a7318bd79ecbbb738070170
kumarAr0631F <nitin.kumar132005@gmail.com>
2025-01-29 13:09:06 +0530

    adding new features

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-200 py-5 items-center">
+    <footer className="bg-gray-200 py-3 items-center">
       {/* Bottom Text */}
       <div className="text-center text-sm text-gray-500 px-4">
         &copy; ICASS-2026. All rights reserved.
