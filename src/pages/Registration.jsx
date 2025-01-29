f92bff635f5cbc6f32216a3e8fe6ad4d9cc2eda4
kumarAr0631F <nitin.kumar132005@gmail.com>
2025-01-29 13:21:10 +0530

    adding new features

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -34,82 +34,79 @@ const Registration = () => {
         <h1 className="text-3xl font-bold text-center mb-8 text-blue-700">
           Registration Details
         </h1>
-        <div className="overflow-x-auto">
-          <div className="mx-auto flex justify-center text-sm">
-            <table className="table-auto w-full max-w-screen-md border border-gray-700 bg-white shadow-lg rounded-lg">
+        <div className="overflow-x-hidden">
+          <div className="mx-auto flex justify-center">
+            <table className="table-auto w-[60%] max-w-[90%] border border-gray-700 bg-white shadow-lg rounded-lg table-layout-fixed">
               <thead className="border border-gray-700">
                 <tr className="bg-gray-200 text-gray-800">
-                  <th className="px-4 py-2 text-center border border-gray-700">
+                  <th className="px-2 py-2 text-center border border-gray-700">
                     Description
                   </th>
-                  <th className="px-4 py-2 text-center border border-gray-700">
+                  <th className="px-2 py-2 text-center border border-gray-700">
                     Type
                   </th>
-                  <th className="px-4 py-2 text-center border border-gray-700">
+                  <th className="px-2 py-2 text-center border border-gray-700">
                     Indian Author (Rs)
                   </th>
-                  <th className="px-4 py-2 text-center border border-gray-700">
+                  <th className="px-2 py-2 text-center border border-gray-700">
                     International Author ($)
                   </th>
                 </tr>
               </thead>
               <tbody className="border border-gray-700">
                 <tr className="border border-gray-700">
-                  <td className="px-4 py-2 border border-gray-700 break-words">
+                  <td className="px-2 py-2 border border-gray-700">
                     Academician/Research Scholar
                   </td>
-                  <td className="px-4 py-2 border text-center border-gray-700">
+                  <td className="px-2 py-2 border text-center border-gray-700">
                     IEEE <br /> NON-IEEE
                   </td>
-                  <td className="px-4 py-2 border text-center border-gray-700">
-                    7000
-                    <br />
+                  <td className="px-2 py-2 border text-center border-gray-700">
+                    7000 <br />
                     8000
                   </td>
-                  <td className="px-4 py-2 border text-center border-gray-700">
+                  <td className="px-2 py-2 border text-center border-gray-700">
                     200 <br /> 250
                   </td>
                 </tr>
                 <tr className="border border-gray-700">
-                  <td className="px-4 py-2 border border-gray-700 break-words">
+                  <td className="px-2 py-2 border border-gray-700">
                     UG/PG Student
                   </td>
-                  <td className="px-4 py-2 border text-center border-gray-700">
+                  <td className="px-2 py-2 border text-center border-gray-700">
                     IEEE <br /> NON-IEEE
                   </td>
-                  <td className="px-4 py-2 border text-center border-gray-700">
+                  <td className="px-2 py-2 border text-center border-gray-700">
                     6000 <br /> 7000
                   </td>
-                  <td className="px-4 py-2 border text-center border-gray-700">
+                  <td className="px-2 py-2 border text-center border-gray-700">
                     100 <br /> 150
                   </td>
                 </tr>
                 <tr className="border border-gray-700">
-                  <td className="px-4 py-2 border border-gray-700 break-words">
-                    Industry
-                  </td>
-                  <td className="px-4 py-2 border text-center border-gray-700">
+                  <td className="px-2 py-2 border border-gray-700">Industry</td>
+                  <td className="px-2 py-2 border text-center border-gray-700">
                     IEEE <br /> NON-IEEE
                   </td>
-                  <td className="px-4 py-2 border text-center border-gray-700">
+                  <td className="px-2 py-2 border text-center border-gray-700">
                     8000 <br /> 9000
                   </td>
-                  <td className="px-4 py-2 border text-center border-gray-700">
+                  <td className="px-2 py-2 border text-center border-gray-700">
                     200 <br /> 250
                   </td>
                 </tr>
 
                 <tr className="border border-gray-700">
-                  <td className="px-4 py-2 border border-gray-700 break-words">
+                  <td className="px-2 py-2 border border-gray-700">
                     Co-Author/Attendee
                   </td>
-                  <td className="px-4 py-2 border text-center border-gray-700">
+                  <td className="px-2 py-2 border text-center border-gray-700">
                     IEEE <br /> NON-IEEE
                   </td>
-                  <td className="px-4 py-2 border text-center border-gray-700">
+                  <td className="px-2 py-2 border text-center border-gray-700">
                     2500
                   </td>
-                  <td className="px-4 py-2 border text-center border-gray-700">
+                  <td className="px-2 py-2 border text-center border-gray-700">
                     75
                   </td>
                 </tr>
