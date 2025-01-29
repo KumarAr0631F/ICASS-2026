af37e5857a77c8cd8f85a6dd0af2ac53e491f9dd
kumarAr0631F <nitin.kumar132005@gmail.com>
2025-01-29 14:48:35 +0530

    adding new features

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,24 +2,23 @@ import { useState } from "react";
 import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  
   return (
-    <div className="flex justify-center text-2xl items-center min-h-screen bg-gray-100">
-      <div className="bg-white shadow-lg rounded-lg p-6">
-        <h2 className="text-2xl font-bold text-blue-700  mb-4">Contact Information</h2>
+    <div className="flex justify-center text-2xl items-center min-h-screen bg-gray-100 px-4 sm:px-6 md:px-8 overflow-hidden">
+      <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-4xl">
+        <h2 className="text-2xl font-bold text-blue-700 mb-4 text-center sm:text-left">Contact Information</h2>
         <table className="table-auto border-collapse border border-gray-300 w-full">
           <tbody>
             <tr>
-              <td className="border border-gray-900 px-4 py-2">For General Enquiries</td>
-              <td className="border border-gray-900 px-4 py-2">icass2026@mru.edu.in</td>
+              <td className="border border-gray-900 px-4 py-2 text-sm sm:text-base">For General Enquiries</td>
+              <td className="border border-gray-900 px-4 py-2 text-sm sm:text-base truncate">icass2026@mru.edu.in</td>
             </tr>
             <tr>
-              <td className="border border-gray-900 px-4 py-2">For Conference related Enquiries</td>
-              <td className="border border-gray-900 px-4 py-2">icass2026@mru.edu.in</td>
+              <td className="border border-gray-900 px-4 py-2 text-sm sm:text-base">For Conference related Enquiries</td>
+              <td className="border border-gray-900 px-4 py-2 text-sm sm:text-base truncate">icass2026@mru.edu.in</td>
             </tr>
             <tr>
-              <td className="border border-gray-900 px-4 py-2">For Paper Submission Enquiries</td>
-              <td className="border border-gray-900 px-4 py-2">icass2026@mru.edu.in</td>
+              <td className="border border-gray-900 px-4 py-2 text-sm sm:text-base">For Paper Submission Enquiries</td>
+              <td className="border border-gray-900 px-4 py-2 text-sm sm:text-base truncate">icass2026@mru.edu.in</td>
             </tr>
           </tbody>
         </table>
