caac6ad7eadcbe13000cf4e22548c1d233184fdc
kumarAr0631F <nitin.kumar132005@gmail.com>
2025-02-06 13:40:39 +0530

    first commit

--- src/pages/AdvisoryCommittee.jsx
+++ src/pages/AdvisoryCommittee.jsx
@@ -19,7 +19,7 @@ const AdvisoryCommittee = () => {
             <li>Prof (Dr) Wen-Cheng Lai, Professor, Dept. of Electrical Eng., Ming Chi University of Technology, Taiwan</li>
             <li>Prof (Dr) Valentina Emilia Balas, Professor, Aurel Vlaicu University of Arad, Romania</li>
             <li>Prof. (Dr) Prashant Jha, Professor, MedTech Innovation and Entrepreneurship, King’s College, London, UK</li>
-            <li>Prof (Dr.) Shipra Suman, Professor, University of London, United Kingdom</li>
+            <li>Prof (Dr) Shipra Suman, Professor, University of London, United Kingdom</li>
             <li>Prof (Dr) Vinod Kumar, Professor, Department of Physics, The University of the West Indies, St. Augustine, Trinidad & Tobago.</li>
             <li>Prof (Dr) Sudipto Ghosh, Professor, Computer Science, Colorado State University, USA</li>
             <li>Prof (Dr) John Martin, Professor, School of Engineering and Computer Science, Jazan University, Saudi Arabia</li>
@@ -28,13 +28,13 @@ const AdvisoryCommittee = () => {
             <li>Dr Muluneh Lemma Woldesemayat, Associate Professor and Scientific Director of Arba Minch Institute of Technology (AMiT), Bsc, Arba Minch University, Ethiopia</li>
             <li>Dr Milkias Berhanu Tuka, Associate Professor, Addis Ababa Science and Technology University, College of Engineering, Ethiopia</li>
             <li>Dr Arindam Pal, Director, Data Science and Optimization, Optym Bangaluru, Conjoint Senior Lecturer, University of New South Wales, Sydney, Australia</li>
-            <li>Dr. Parnika Gupta, Senior Engineer, Global Foundries, Belgium</li>
-            <li>Dr. Promod Thakur, Assistant Professor, Hubert Curien Laboratory, Jean Monnet University in Saint-Etienne, France.</li>
-            <li>Dr. Mohit Mittal, Data Scientist, Shiratech-Knowtion GmgH, Germany</li>
-            <li>Dr. Jae Yeon Park, Senior Researcher, Korea Atomic Energy Research Institute, Daejeon, South Korea</li>
+            <li>Dr Parnika Gupta, Senior Engineer, Global Foundries, Belgium</li>
+            <li>Dr Promod Thakur, Assistant Professor, Hubert Curien Laboratory, Jean Monnet University in Saint-Etienne, France.</li>
+            <li>Dr Mohit Mittal, Data Scientist, Shiratech-Knowtion GmgH, Germany</li>
+            <li>Dr Jae Yeon Park, Senior Researcher, Korea Atomic Energy Research Institute, Daejeon, South Korea</li>
             <li>Dr Mohammad Ali Jallal, Research Fellow, French Alternative Energies and Atomic Energy Commission (CEA), Paris-Saclay, France</li>
             <li>Dr Ilyos Rabbimov, Researcher, Center for Economic Research and Reform under the Administration of the President of the Republic of Uzbekistan, Tashkent, Uzbekistan</li>
-            <li>Mr. Prashant Gupta, Software Development Manager, Amazon, USA</li>
+            <li>Mr Prashant Gupta, Software Development Manager, Amazon, USA</li>
           </ul>
         </div>
 
@@ -46,7 +46,7 @@ const AdvisoryCommittee = () => {
             <li>Prof (Dr) Bhim Singh, Professor, IIT Delhi, Delhi</li>
             <li>Prof (Dr) MN Hooda, Director, Bharati Vidyapeeth's Institute of Computer Applications and Management, Delhi</li>
             <li>Prof (Dr) Anjana Gosain, Professor and Dean, USICT, GGSIPU, New Delhi</li>
-            <li>Prof. (Dr) Prerna Gaur, Professor, NSUT, New Delhi</li>
+            <li>Prof (Dr) Prerna Gaur, Professor, NSUT, New Delhi</li>
             <li>Prof (Dr) Ranganath. M. Singari, Professor, Department of Mechanical Engineering, DTU</li>
             <li>Prof (Dr) MPS Bhatia, Professor, NSUT, New Delhi</li>
             <li>Prof (Dr) Vasudha Bhatnagar, Professor, DTU, New Delhi</li>
@@ -56,8 +56,8 @@ const AdvisoryCommittee = () => {
             <li>Prof (Dr) I. K. Bhat, Ex VC, Manav Rachna University, Faridabad</li>
             <li>Prof (Dr) M. Emran Khan, Professor, Department of Mechanical Engineering, JMI, Delhi</li>
             <li>Prof (Dr) B Balamurugan, Professor and Dean Academics, Shiv Nadar University, Noida</li>
-            <li>Mr. Sanjay Jain, Head of Google for Education, India</li>
-            <li>Mr. Anil Kumar Saini, Scientist, CEERI, Pilani</li>
+            <li>Mr Sanjay Jain, Head of Google for Education, India</li>
+            <li>Mr Anil Kumar Saini, Scientist, CEERI, Pilani</li>
             <li>Prof (Dr) Amrita Rai, Professor, Lloyd College, Greater Noida</li>
           </ul>
         </div>
